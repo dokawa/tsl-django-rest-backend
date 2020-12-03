3e848eb786da0e335867115a29188359d1254904
dokawa <daniel.okawa@gmail.com>
2020-12-02 22:01:10 -0300

    [feature] Add create user

--- wall_app/serializers.py
+++ wall_app/serializers.py
@@ -2,7 +2,18 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
+    # user = serializers.ModelSerializer()
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}, "email": {'required': True}, "username": {'required':True} }
+
+    # def create(self, data):
+    #     user = User(**data)
+    #     user.set_password(data['password'])
+    #     user.save()
+    #     return user
+
