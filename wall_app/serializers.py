0a365b11b68e351681df661cf9302e6ae72c4c05
dokawa <daniel.okawa@gmail.com>
2020-12-02 22:03:01 -0300

    [clean] Remove unnused code on serializers.py

--- wall_app/serializers.py
+++ wall_app/serializers.py
@@ -4,16 +4,9 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
-    # user = serializers.ModelSerializer()
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}, "email": {'required': True}, "username": {'required':True} }
+        extra_kwargs = {'password': {'write_only': True}, "email": {'required': True}, "username": {'required': True}}
 
-    # def create(self, data):
-    #     user = User(**data)
-    #     user.set_password(data['password'])
-    #     user.save()
-    #     return user
 
