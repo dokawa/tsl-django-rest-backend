4755aa67b95eb1893498b51f1362b8242dba0f80
dokawa <daniel.okawa@gmail.com>
2020-12-14 08:47:24 -0300

    [fix] Add localhost to cors config

--- django_rest_backend/settings.py
+++ django_rest_backend/settings.py
@@ -148,11 +148,11 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# CORS_ORIGIN_REGEX_WHITELIST = (r'https?://localhost[:\d+]?', r'https?://tsl-django-backend.herokuapp.com.*')
+CORS_ORIGIN_REGEX_WHITELIST = (r'https?://localhost[:\d+]?', r'https?://tsl-react-frontend.herokuapp.com.*')
 # CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://tsl-react-frontend.herokuapp.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://tsl-react-frontend.herokuapp.com", "http://localhost:3000"
+# ]
 
 config = configparser.ConfigParser()
 config.read('credentials/credentials')
