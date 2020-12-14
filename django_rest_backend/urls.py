87facdc857561dae0ced384ad1dc3ec9448a4a9b
dokawa <daniel.okawa@gmail.com>
2020-12-14 20:40:57 -0300

    [fix] Move urls to correct files

--- django_rest_backend/urls.py
+++ django_rest_backend/urls.py
@@ -13,34 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
-from rest_framework.authtoken.views import ObtainAuthToken
-
-from wall_app import views
-from wall_app.views import UserViewSet, WallPostViewSet
-
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-wall_post_list = WallPostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-wall_post_create = WallPostViewSet.as_view({
-    'get': 'retrieve',
-    'post': 'create'
-})
 
 urlpatterns = [
-    path('', wall_post_list, name='wall-post-detail'),
-    # path('admin/', admin.site.urls),
-    path('message/', wall_post_list, name='wall-post-list'),
-    path('message/<int:pk>/', wall_post_create, name='wall-post-create'),
-    # path('users/', user_list, name='user-list'),
-    path('register/', views.UserCreate.as_view()),
-    path('register-as-guest/', views.AnonymousUserCreate.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
-    path('token/', ObtainAuthToken.as_view()),
+    path('', include('wall_app.urls')),
 ]
+
+
--- wall_app/urls.py
+++ wall_app/urls.py
@@ -1,13 +1,30 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import ObtainAuthToken
+
 from wall_app import views
+from wall_app.views import UserViewSet, WallPostViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+wall_post_list = WallPostViewSet.as_view({
+    'get': 'list',
+})
+wall_post_create = WallPostViewSet.as_view({
+    'get': 'retrieve',
+    'post': 'create'
+})
+
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('', wall_post_list, name='wall-post-detail'),
+    # path('admin/', admin.site.urls),
+    path('message/', wall_post_list, name='wall-post-list'),
+    path('message/<int:pk>/', wall_post_create, name='wall-post-create'),
+    # path('users/', user_list, name='user-list'),
+    path('register/', views.UserCreate.as_view()),
+    path('register-as-guest/', views.AnonymousUserCreate.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', ObtainAuthToken.as_view()),
 ]
