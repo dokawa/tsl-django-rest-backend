f2790268b2918292ccb1ca021971bcd339eb9cdd
dokawa <daniel.okawa@gmail.com>
2020-12-14 14:15:33 -0300

    [feature] Add D. Trump post

--- django_rest_backend/settings.py
+++ django_rest_backend/settings.py
@@ -86,13 +86,6 @@ WSGI_APPLICATION = 'django_rest_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
@@ -149,10 +142,6 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_REGEX_WHITELIST = (r'https?://localhost[:\d+]?', r'https?://tsl-react-frontend.herokuapp.com.*')
-# CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "https://tsl-react-frontend.herokuapp.com", "http://localhost:3000"
-# ]
 
 config = configparser.ConfigParser()
 config.read('credentials/credentials')
